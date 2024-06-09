eaf7d58417d1121609787b044483149f8bb6ace5
Utkarsh Singh <utkarshsingh.5474@gmail.com>
2024-06-09 13:33:14 +0530

    chore: Update API_URL to use production backend API URL

--- src/utils/api.tsx
+++ src/utils/api.tsx
@@ -1,4 +1,4 @@
-const API_URL = process.env.NEXT_PUBLIC_LOCAL_BACKEND_API_URL || 'http://localhost:5000/api';
+const API_URL = process.env.NEXT_PUBLIC_PROD_BACKEND_API_URL || 'http://localhost:5000/api';
 
 export const fetchCampaigns = async () => {
   try {
